d09135ea0bcb05c44c0a3ebd5ef2ee8378962cc9
Wani <devwani93@gmail.com>
2021-08-17 23:14:50 +0900

    3.5 좌표 게임 💡

--- 2-types-projects/game.ts
+++ 2-types-projects/game.ts
@@ -1,6 +1,32 @@
 /**
  * Let's make a game 🕹
  */
+interface Position {
+  x: number;
+  y: number;
+}
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+const position: Position = { x: 0, y: 0 };
+const move = (dir: Direction) => {
+  switch (dir) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`unknown direction: ${dir}`);
+  }
+};
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
