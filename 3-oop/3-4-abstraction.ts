e03720a7400a750278f05e44cb250ef8627900e5
Wani <devwani93@gmail.com>
2021-08-24 23:20:31 +0900

    4.12 Interface 인터페이스, 모든것의 시작

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -13,7 +13,13 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  class CoffeeMachine implements CoffeeMaker {
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(beans: number): void;
+    clean(): void;
+  }
+
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
     private static BEANS_GRAMM_PER_SHOT: number = 7; // class level
     private coffeeBeans: number = 0; // instance (object) level
 
@@ -58,13 +64,43 @@
       }
       this.coffeeBeans += beans;
     }
+
+    clean() {
+      console.log('cleaning the machine...🛁');
+    }
   }
 
   const coffeeMachine: CoffeeMachine = CoffeeMachine.makeMachine(22);
   coffeeMachine.fillCoffeeBeans(10);
   coffeeMachine.makeCoffee(2);
 
-  const coffeeMachine2: CoffeeMaker = CoffeeMachine.makeMachine(22);
+  // 인터페이스에서 정의된 속성, 메서드만 사용 가용
+  const coffeeMachine2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(22);
   // coffeeMachine2.fillCoffeeBeans(10); CoffeeMaker에는 fillCoffeeBeans 메서드가 없음.
+  coffeeMachine2.fillCoffeeBeans(32);
   coffeeMachine2.makeCoffee(2);
+  coffeeMachine2.clean();
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      this.machine.fillCoffeeBeans(45);
+      this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32));
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  amateur.makeCoffee(); // 같은 인스턴스를 받았지만 두가지 인터페이스를 구현하기 때문에 CoffeeMaker, CommercialCoffeeMaker로 나뉜다.  
+  pro.makeCoffee();     // 인터페이스에 규약된 함수들만 접근 가능하기 때문에 사용자는 내부 구현을 몰라도 사용 가능.
 }
